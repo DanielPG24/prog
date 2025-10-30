60a10f79a024618bfd8ba32d856b6cd7109750ed
DanielPG24 <dpingom1604@g.educaand.es>
2025-10-30 08:47:34 +0100

    Delete sumar bucle.py

--- sumar bucle.py
+++
@@ -1,8 +0,0 @@
-suma = 0
-while True:
-    num = int(print ("Introduzca el otro número (pulse 0 para terminar): "))
-    if num == 0:
-        break
-    suma += num
-print ("La suma es: ", suma)
-    
