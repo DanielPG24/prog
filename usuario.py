2f71e4d722e71b5e30856a7baeeb42c3181ec4b4
DanielPG24 <dpingom1604@g.educaand.es>
2025-10-30 08:48:14 +0100

    Delete usuario.py

--- usuario.py
+++
@@ -1,6 +0,0 @@
-usuario = input ("Escriba el usuario: ")
-clave = input ("Escriba su contraseña: ")
-longitud = len(clave)
-while (len(clave) < 8):
-    clave = input ("Escriba su contraseña: ")
-print("Bienvenido!")
