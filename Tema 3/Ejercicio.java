d1a5d1929c16b0ce23cb8bf207c9133e8fa73ce7
DanielPG24 <dpingom1604@g.educaand.es>
2025-12-02 13:28:41 +0100

    Add files via upload

---
+++ Tema 3/Ejercicio.java
@@ -0,0 +1,35 @@
+public class HolaMundo 
+{
+	public static void main(String[] args)
+	{
+		// Declarar variables
+		boolean boleano;
+		char caracter;
+		byte bite;
+		short corto;
+		int entero;
+		long largo;
+		float decimal
+		double doble;
+		
+		//Inicializar variables
+		boleano = true;
+		caracter = a;
+		bite = 1;
+		corto = 23345;
+		entero = 10;
+		largo = 123456;
+		decimal = 1.20;
+		doble = 22342.53657;
+
+		//Imprimir por consola
+		System.out.println(boleano);
+		System.out.println(caracter);
+		System.out.println(bite);
+		System.out.println(corto);
+		System.out.println(entero);
+		System.out.println(largo);
+		System.out.println(decimal);
+		System.out.println(doble);
+	}
+}
